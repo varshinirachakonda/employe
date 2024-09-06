435acbe7a5b01e88e31faf1c8aef840858f007a2
varshinirachakonda <varshinirachakonda8@gmail.com>
2024-09-06 21:18:05 +0530

    first commit

---
+++ employee/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from salary.models import Employee
+def information(request):
+    employees=Employee.objects.all()
+    context={
+        'employees':employees,
+    }
+    return render(request,'home.html',context)
---
+++ salary/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from salary.models import Employee
+# Register your models here.
+from salary.models import Employee
+admin.site.register(Employee)
